a058457943ea700aaf66453f4e9661ee04b7a362
Jörg Zeipelt <Jrg Zeipelt>
2016-10-01 16:06:34 +0200

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var Navigation = require('Navigation');
+
+var Main = (props) => {
+  return (
+    <div>
+      <Navigation/>
+      <p>Main.jsx rendered</p>
+      {props.children}
+    </div>
+  );
+};
+
+module.exports = Main;
