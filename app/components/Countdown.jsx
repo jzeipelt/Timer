1a0291a4656dfac0b03ffabe9b3b9903d1b21702
Jörg Zeipelt <Jrg Zeipelt>
2016-10-10 14:11:47 +0200

    Add Navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Countdown = () => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Countdown</h1>
+      <p className="text-center">Some example text.</p>
+    </div>
+  );
+};
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Timer = () => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Timer</h1>
+      <p className="text-center">Some timer text.</p>
+    </div>
+  );
+};
+
+module.exports = Timer;
